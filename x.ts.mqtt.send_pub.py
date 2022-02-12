d96864679d96585944caa8e9cea9fe98e73d6690
smartcomputerlab <89449454+smartcomputerlab@users.noreply.github.com>
2022-02-12 15:22:58 +0100

    Add files via upload

---
+++ x.ts.mqtt.send_pub.py
@@ -0,0 +1,22 @@
+from umqtt.simple import MQTTClient
+import wifista
+import time
+server = "mqtt.thingspeak.com"
+client = MQTTClient("umqtt_client", server)
+CHANNEL_ID = "1626377"
+WRITE_API_KEY = "3IN09682SQX3PT4Z"
+topic = "channels/" + CHANNEL_ID + "/publish/" + WRITE_API_KEY
+
+temp =21.5
+hum =55.7
+
+for i in range(60):
+    wifista.connect()
+    payload = "field1="+str(temp)+"&field2="+str(hum)
+    client.connect()
+    client.publish(topic, payload)
+    client.disconnect()
+    temp=temp+1.0
+    hum=hum+2.0
+    time.sleep(15)
+
